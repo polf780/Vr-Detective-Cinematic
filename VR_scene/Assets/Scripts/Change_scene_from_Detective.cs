8290dcc5cd302ce84fbed45bcd8da0a37ecb60d7
polf780 <47743753+polf780@users.noreply.github.com>
2022-04-25 20:47:41 +0200

    Scene one has buttnon to change map, working well

---
+++ VR_scene/Assets/Scripts/Change_scene_from_Detective.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change_scene_from_Detective : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void changemap()
+    {
+        //SceneManager.LoadScene("Escena Asesinato con Tren");
+        SceneManager.LoadScene("Escena Asesinato con Tren");
+    }
+}
