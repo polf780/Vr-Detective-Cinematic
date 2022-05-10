dc7bb6ace4122864e9189c4a817475af9d4e2782
polf780 <47743753+polf780@users.noreply.github.com>
2022-05-10 13:28:51 +0200

    Added script for controling lights in last scene

---
+++ VR_scene/Assets/Scripts/Lightning_selecting_suspects.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning_selecting_suspects : MonoBehaviour
+{
+    public GameObject Murloc_Light;
+    public GameObject Faun_Light;
+    public GameObject Armor_Light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     public void Enable_light_Murloc()
+    {
+        Murloc_Light.SetActive(true);
+    }
+    public void Close_light_Murloc()
+    {
+        Murloc_Light.SetActive(false);
+    }
+
+    public void Faun_light_Enable()
+    {
+        Faun_Light.SetActive(true);
+    }
+    public void Faun_light_Close()
+    {
+        Faun_Light.SetActive(false);
+    }
+
+    public void Armor_light_Enable()
+    {
+        Armor_Light.SetActive(true);
+    }
+    public void Armor_light_Close()
+    {
+        Armor_Light.SetActive(false);
+    }
+}
