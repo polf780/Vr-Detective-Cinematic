f916e4fa2c3ec70c9d923fcac5dfec21477932ee
polf780 <47743753+polf780@users.noreply.github.com>
2022-05-31 21:54:24 +0200

    added animation timeline 1 with more audios
    deleting old animations and some bnad

--- VR_scene/Assets/Scripts/Start_robot_idle.cs
+++ VR_scene/Assets/Scripts/Start_robot_idle.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Playables;
 public class Start_robot_idle : MonoBehaviour
 {
-    public GameObject Robotidle;
+    public PlayableDirector Director_idle_robot;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +14,11 @@ public class Start_robot_idle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public void Start_idlerobot_animation()
+    public void Play_director_robot_idle()
     {
-        Robotidle.GetComponent<Animator>().Play("Take 001");
+        Director_idle_robot.Play();
+
     }
 }
